94315ae6e2c02d24d428c4abca67536e997a5ea7
talha0404 <67678204+talha0404@users.noreply.github.com>
2021-11-18 00:25:15 +0300

    Add project files.

---
+++ MobileBlogXamarin/MobileBlogXamarin/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using MobileBlogXamarin.ViewModels;
+using MobileBlogXamarin.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace MobileBlogXamarin
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ MobileBlogXamarin/MobileBlogXamarin/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using MobileBlogXamarin.Models;
+using MobileBlogXamarin.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MobileBlogXamarin.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
